3b7e1e7020e772e9e5b6b8ff3efb58ae2137348d
Pablo <pablo@US-IT00822.local>
2020-04-04 20:07:50 -0700

    The burger displays ingredients dynamically using the ingredients state on the BurgerBuilder

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,27 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const propsIngredients = props.ingredients;
+
+    let burgerIngredients = Object.keys(propsIngredients).map((ingKey) => {
+        return [...new Array(propsIngredients[ingKey])].map((_, i) => {
+            return <BurgerIngredient type={ingKey} key={ingKey+i}></BurgerIngredient>
+        });
+    }).reduce((finalArray, arrayPerKey) => {
+        // Reducing to flatten the array... easier to calculate the amount
+        return finalArray.concat(arrayPerKey);
+    }, []);
+
+    if (burgerIngredients.length === 0) {
+        burgerIngredients = <p>Please start adding ingredients!</p>
+    }
+ 
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top'></BurgerIngredient>
-            <BurgerIngredient type='cheese'></BurgerIngredient>
-            <BurgerIngredient type='meat'></BurgerIngredient>
+
+            {burgerIngredients}
+
             <BurgerIngredient type='bread-bottom'></BurgerIngredient>
         </div>
     );
