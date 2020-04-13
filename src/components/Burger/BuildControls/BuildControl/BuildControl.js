dd6b63b435c82e6ef438939130842e94ca053072
Daniela marin <danielamarin@Globants-MacBook-Pro.local>
2020-04-13 15:57:19 -0500

    added data attribute to track event in ingredients

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import classes from './BuildControl.module.css'
 
-const buildControl = (props) => (
-    <div className={classes.BuildControl} >
-        <div className={classes.Label}>{props.label}</div>
-        
-        <button
-            className={classes.Less}
-            onClick={props.removeIngredient}
-            disabled={props.disabled}>
-            Less
-        </button>
+const buildControl = (props) => {
+    const propDataAdd = { ['data-add-'+props.label] : props.label };
+    const propDataRemove = { ['data-remove-'+props.label] : props.label };
 
-        <button
-            className={classes.More}
-            onClick={props.addIngredient}>
-            More
-        </button>
-    </div>
-);
+    return (
+        <div className={classes.BuildControl} >
+            <div className={classes.Label}>{props.label}</div>
+            
+            <button
+                {...propDataRemove}
+                className={classes.Less}
+                onClick={props.removeIngredient}
+                disabled={props.disabled}>
+                Less
+            </button>
 
+            <button
+                {...propDataAdd}
+                className={classes.More}
+                onClick={props.addIngredient}>
+                More
+            </button>
+        </div>
+    );
+}
 export default buildControl;
